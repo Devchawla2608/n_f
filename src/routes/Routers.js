54b3c1f84133f9adbbae9dff2e3be52f2582ac2b
Deepanshu Chawla <dev.chawla2608@gmail.com>
2023-08-12 12:47:07 +0530

    3

--- src/routes/Routers.js
+++ src/routes/Routers.js
@@ -2,24 +2,24 @@ import React from "react";
 
 import { Routes, Route} from "react-router-dom";
 
-// import Home from "../pages/Home";
-// import Market from "../pages/Market";
-// import Create from "../pages/Create";
-// import Contact from "../pages/Contact";
+import Home from "../pages/Home";
+import Market from "../pages/Market";
+import Create from "../pages/Create";
+import Contact from "../pages/Contact";
 import SignIn from "../components/SignIn/SignIn.jsx";
-// import SignUp from "../components/SignUp/SignUp.jsx";
-// import Wallet from "../pages/Wallet";
-// import NftDetails from "../pages/NftDetails";
-// import Explore from "../pages/Explore.jsx";
-// import Profile from "../pages/Profile.jsx";
-// import Post from "../pages/Post.jsx";
-// import PrivateRoute from "./PrivateRoute.jsx";
+import SignUp from "../components/SignUp/SignUp.jsx";
+import Wallet from "../pages/Wallet";
+import NftDetails from "../pages/NftDetails";
+import Explore from "../pages/Explore.jsx";
+import Profile from "../pages/Profile.jsx";
+import Post from "../pages/Post.jsx";
+import PrivateRoute from "./PrivateRoute.jsx";
 
 const Routers = () => {
   return (
     <Routes>
       <Route path="/" element={<SignIn/>} />
-      {/* <Route path="/sign-up" element={ <SignUp/>} />
+      <Route path="/sign-up" element={ <SignUp/>} />
       <Route path="/home" element={       <PrivateRoute> <Home />  </PrivateRoute> } />
       <Route path="/market" element={<PrivateRoute> <Market />  </PrivateRoute>} />
       <Route path="/create" element={<PrivateRoute> <Create />  </PrivateRoute>} />
@@ -28,7 +28,7 @@ const Routers = () => {
       <Route path="/market/:id" element={<PrivateRoute> <NftDetails />  </PrivateRoute>} />
       <Route path="/Explore" element={<PrivateRoute> <Explore />  </PrivateRoute>} />
       <Route path="/Profile" element={<PrivateRoute> <Profile />  </PrivateRoute>} />
-      <Route path="/post" element={<PrivateRoute> <Post />  </PrivateRoute>} /> */}
+      <Route path="/post" element={<PrivateRoute> <Post />  </PrivateRoute>} />
     </Routes>
   );
 };
